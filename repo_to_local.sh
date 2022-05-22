2bec8895a814211d0c816e9c6f600da7aa9541ee
Daniel Mossaband <danmossa@gmail.com>
2022-05-21 21:46:43 -0700

    Specialize

--- repo_to_local.sh
+++ repo_to_local.sh
@@ -5,15 +5,23 @@ CURRENTDATE=$(date +%s)
 mkdir ~/dotfiles.backup.${CURRENTDATE}
 
 mv ~/.p10k.zsh ~/dotfiles.backup.${CURRENTDATE}
-mv ~/.personal.public.zsh ~/dotfiles.backup.${CURRENTDATE}
-mv ~/.work.public.zsh ~/dotfiles.backup.${CURRENTDATE}
 mv ~/.zshenv ~/dotfiles.backup.${CURRENTDATE}
 mv ~/.zshrc ~/dotfiles.backup.${CURRENTDATE}
+mv ~/.personal.public.zsh ~/dotfiles.backup.${CURRENTDATE}
+mv ~/.work.public.zsh ~/dotfiles.backup.${CURRENTDATE}
+
 
 cp ./.p10k.zsh ~/.p10k.zsh
-cp ./.personal.public.zsh ~/.personal.public.zsh
-cp ./.work.public.zsh ~/.work.public.zsh
 cp ./.zshenv ~/.zshenv
 cp ./.zshrc ~/.zshrc
+
+echo "Is this Personal or Work?"
+select pw in "Personal" "Work"; do
+    case $pw in
+        Personal ) cp ./.personal.public.zsh ~/.personal.public.zsh; break;;
+        Work ) cp ./.work.public.zsh ~/.work.public.zsh; break;;
+    esac
+done
+
 
 exec zsh
